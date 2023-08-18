246524af3bde098caa964378aedf1695224eff6e
SpicySpiy <tigereli100@gmail.com>
2023-08-18 09:23:01 +0200

    My first commit :). Here's the code

---
+++ hangman.py
@@ -0,0 +1,37 @@
+#directily copy pasted from
+#https://github.com/calthoff/tstp/blob/master/part_I/bringing_it_all_together/challenges/chap10_challenge1.py
+
+import random
+
+
+def hangman():
+    word_list = ["Python", "Java", "computer", "hacker", "painter"]
+    random_number = random.randint(0, 4)
+    word = word_list[random_number]
+    wrong_guesses = 0
+    stages = ["", "________      ", "|      |      ", "|      0      ", "|     /|\     ", "|     / \     ", "|"]
+    remaining_letters = list(word)
+    letter_board = ["__"] * len(word)
+    win = False
+    print('Welcome to Hangman')
+    while wrong_guesses < len(stages) - 1:
+        print('\n')
+        guess = input("Guess a letter")
+        if guess in remaining_letters:
+            character_index = remaining_letters.index(guess)
+            letter_board[character_index] = guess
+            remaining_letters[character_index] = '$'
+        else:
+            wrong_guesses += 1
+        print((' '.join(letter_board)))
+        print('\n'.join(stages[0: wrong_guesses + 1]))
+        if '__' not in letter_board:
+            print('You win! The word was:')
+            print(' '.join(letter_board))
+            win = True
+            break
+    if not win:
+        print('\n'.join(stages[0: wrong_guesses]))
+        print('You lose! The words was {}'.format(word))
+
+hangman()
